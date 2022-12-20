bfd704b86b6c46820c1500d0f422a1699244a948
Paul Jung <pbnjcub@gmail.com>
2022-12-20 08:43:59 -0500

    lab done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
+    if array.length == 1
+        array[0]
+    elsif array.length == 2
+        array.join(" and ")
+    elsif array.length == 3
+        newArray = array.insert(1, ', ').insert(3, ', ').insert(4, 'and ')
+        newArray.join()
+    elsif array.length > 3
+        array.each_with_index do |string, index|
+            if index < array.length - 1
+                array[index] << ", "
+            elsif index == array.length - 1
+                array[index].prepend("and ")
+            end
+        end
+        array.join()
+    end
+
+
 
 end
