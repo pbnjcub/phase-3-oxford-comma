d7241e6d16ba655a0c890a9dc55b46957ddf0bbf
Paul Jung <pbnjcub@gmail.com>
2023-02-09 19:40:42 -0500

    commit tree 07219acce44f4a78c449f7a050661c280792eda1

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,7 @@ def oxford_comma(array)
     elsif array.length == 2
         array.join(" and ")
     elsif array.length == 3
-        newArray = array.insert(1, ', ').insert(3, ', ').insert(4, 'and ')
-        newArray.join()
+        array.insert(1, ', ').insert(3, ', ').insert(4, 'and ').join()
     elsif array.length > 3
         array.each_with_index do |string, index|
             if index < array.length - 1
@@ -16,7 +15,51 @@ def oxford_comma(array)
         end
         array.join()
     end
-
-
-
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def oxford_comma(array)
+#     if array.length == 1
+#         array[0]
+#     elsif array.length == 2
+#         array.join(" and ")
+#     elsif array.length == 3
+#         newArray = array.insert(1, ', ').insert(3, ', ').insert(4, 'and ')
+#         newArray.join()
+#     elsif array.length > 3
+#         array.each_with_index do |string, index|
+#             if index < array.length - 1
+#                 array[index] << ", "
+#             elsif index == array.length - 1
+#                 array[index].prepend("and ")
+#             end
+#         end
+#         array.join()
+#     end
+
+
+
+# end
